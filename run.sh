aab019ece8c6a7b5b123885b3978eaf91fa47d57
wanwarr <165036072+wanwarr@users.noreply.github.com>
2024-03-26 20:01:31 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-c617fa65-8c7c-4421-a5ef-e2984caace90/)
 
 
 echo "$curl_output" >> Update.tsx
